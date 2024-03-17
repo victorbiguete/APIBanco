ce06378b56511803d8f3b41699f8f17e8bf60c60
Rannatpf <rannatpf@gmail.com>
2024-03-17 18:20:10 -0300

    sacar/depositar

---
+++ Controllers/Model/Cliente.cs
@@ -0,0 +1,24 @@
+﻿namespace APIBanco.Controllers.Model
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+
+        public int AnoNascimento { get; private set; }
+        public string Cpf { get; private set; }
+        public List<Conta> Contas { get; set; }
+
+        public Cliente(string nome, string cpf, int anoNascimento)
+        {
+            if (Int32.Parse(DateTime.Now.ToString("yyyy")) - anoNascimento < 18)
+                throw new System.ArgumentException("O cliente deve ter mais de 18 anos");
+            if (cpf.Length != 11)
+                throw new System.ArgumentException("O CPF deve possuir 11 digitos!");
+
+            Nome = nome;
+            AnoNascimento = anoNascimento;
+            Cpf = cpf;
+        }
+    }
+}
+
---
+++ Controllers/Model/ContaPoupanca.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace APIBanco.Controllers.Model
+{
+    public class ContaPoupanca
+    {
+        private Conta conta;
+        private decimal saldoPoupanca;
+        private int aniversario;
+
+        public ContaPoupanca(Conta conta, decimal saldoPoupanca, int aniversario)
+        {
+            this.conta = conta;
+            this.saldoPoupanca = saldoPoupanca;
+            this.aniversario = aniversario;
+        }
+
+        public Conta GetConta()
+        {
+            return conta;
+        }
+
+        public void SetConta(Conta conta)
+        {
+            this.conta = conta;
+        }
+
+        public decimal GetSaldoPoupanca()
+        {
+            return saldoPoupanca;
+        }
+
+        public void SetSaldoPoupanca(decimal saldoPoupanca)
+        {
+            this.saldoPoupanca = saldoPoupanca;
+        }
+
+        public int GetAniversario()
+        {
+            return aniversario;
+        }
+
+        public void SetAniversario(int aniversario)
+        {
+            this.aniversario = aniversario;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            saldoPoupanca += valor;
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if (saldoPoupanca >= valor)
+            {
+                saldoPoupanca -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente na poupança.");
+                return false;
+            }
+        }
+
+    }
+}
