7b7bef1e86ec3da3e47c33327fb98568968a7aef
DandaraF <dandara-chr@hotmail.com>
2024-03-24 18:00:42 -0300

    correção da tipagem dos atributos

--- Domain/Dtos/CreditCardRequestDto.cs
+++ Domain/Dtos/CreditCardRequestDto.cs
@@ -2,10 +2,10 @@
 {
     public class CreditCardRequestDto
     {
-        public string CardNumber { get; set; }
-        public string HolderName { get; set; }
+        public string? CardNumber { get; set; }
+        public string? HolderName { get; set; }
         public DateTime ExpiryDate { get; set; }
-        public string CVV { get; set; }
+        public string? CVV { get; set; }
         public decimal TotalLimit { get; set; }
     }
 }
--- Domain/Models/CreditCard.cs
+++ Domain/Models/CreditCard.cs
@@ -3,12 +3,12 @@
     public class CreditCard
     {
         public int Id { get; set; }
-        public ulong CardNumber { get; set; } // Número do cartão (hipotético)
-        public string? HolderName { get; set; } // Nome do titular do cartão
-        public DateTime ExpiryDate { get; set; } // Data de validade do cartão
-        public string? CVV { get; set; } // Código de segurança do cartão
-        public decimal TotalLimit { get; set; } // Limite total do cartão
-        public decimal UsedLimit { get; set; } // Limite utilizado do cartão
+        public ulong CardNumber { get; set; } 
+        public string? HolderName { get; set; }
+        public DateTime ExpiryDate { get; set; } 
+        public string? CVV { get; set; }
+        public decimal TotalLimit { get; set; }
+        public decimal UsedLimit { get; set; }
         public virtual List<CardTransaction>? CardTransactions { get; set; }
     }
 }
