ea8874edf38a8f235df33468dabcd4ae70e71671
mmerga <78653901+mmerga@users.noreply.github.com>
2024-03-24 20:34:45 -0300

    fix where number and cep not save on sign up

--- Domain/Dtos/AdressResponseDto.cs
+++ Domain/Dtos/AdressResponseDto.cs
@@ -3,9 +3,9 @@ namespace APIBanco.Domain.Dtos;
 public class AdressResponseDto
 {
     public int Id { get; set; }
-    public int cep { get; set; }
+    public int Cep { get; set; }
     public string Street { get; set; } = null!;
-    public int number { get; set; }
+    public int Number { get; set; }
     public string Neighborhood { get; set; } = null!;
     public string City { get; set; } = null!;
     public string State { get; set; } = null!;
